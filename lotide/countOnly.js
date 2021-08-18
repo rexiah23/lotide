1d781b7679420488b1f690340157f6b87bb3c463
Lewis Lee <lewisforbusiness@gmail.com>
2021-08-18 12:41:12 -0400

    Added countOnly.js

---
+++ lotide/countOnly.js
@@ -0,0 +1,41 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  const countObj = {};
+  
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (!countObj[item]) {
+        countObj[item] = 1;
+      } else {
+        countObj[item] ++;
+      }
+    }
+  }
+  return countObj;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
