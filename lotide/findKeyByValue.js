7613f702716f5bb01f372c15f1fc5808334500b9
Lewis Lee <lewisforbusiness@gmail.com>
2021-08-18 13:50:32 -0400

    Added findKeyByValue.js

---
+++ lotide/findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, value) => {
+  
+  for (const key of Object.keys(obj)) {
+    const item = obj[key];
+
+    if (item === value) {
+      return key;
+    }
+  }
+
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
