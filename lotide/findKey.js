4fe38d761ed4e8207e26917b7fd5d8fce6249ad4
Lewis Lee <lewisforbusiness@gmail.com>
2021-08-19 14:47:39 -0400

    Added findKey.js

---
+++ lotide/findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKey = (obj, callback) => {
+
+  for (const key of Object.keys(obj)) {
+    const item = obj[key];
+    if (callback(item)) {
+      return key
+    }
+  }
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
