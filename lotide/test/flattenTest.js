cb59941671f2d789a9e3859ddbad3a9e05dc115d
Lewis Lee <lewisforbusiness@gmail.com>
2021-08-23 15:19:09 -0400

    Made changes

---
+++ lotide/test/flattenTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const flatten = require('../flatten');
+
+describe('flatten.js', () => {
+  it('should return [1,2,3,4] for [1,2,[3,4]]', () => {
+    assert.deepEqual(flatten([1,2,[3,4]]), [1,2,3,4]); 
+  });
+
+  it('should return [1,2,3,4,5] for [1,[2,3,[4]], 5]', () => {
+    assert.deepEqual(flatten([1,[2,3,[4]], 5]), [1,2,3,4,5]); 
+  });
+})
+
+
--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,14 +1,14 @@
 const assert = require('chai').assert; 
 const tail = require('../tail');
 
-// assertArraysEqual(tail([1]), []);
-// assertArraysEqual(tail([]), []);
-// assertArraysEqual(tail([1,2,3,4]), [2,3,4]);
-
 describe("tail.js", () => {
   
   it("should return [] for [1]", () => {
     assert.deepEqual(tail([1]), []);
+  });
+
+  it("should return [2,3,4] for [1,2,3,4]", () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4]);
   });
 })
 
